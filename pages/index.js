c65e38ee1d4f81f24824aca20541ebd718268bc6
manfuji <telebrytt@gmail.com>
2022-10-21 09:22:59 +0000

    don't have otp

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,7 @@ const Home = () => {
           </div>
           <div>
             <span className="cursor-pointer text-gray-700">
-              Dodn't have OTP?{' '}
+              Don't have OTP?{' '}
               {/* <a
                 className="text-blue-500"
                 onClick={(e) => {
